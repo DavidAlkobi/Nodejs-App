a3b6b723cff2260f4fce363481f9ca562a8ac99b
DavidAlkobi <136454939+DavidAlkobi@users.noreply.github.com>
2023-09-18 13:01:51 +0300

    Update server.js

--- app/server.js
+++ app/server.js
@@ -2,23 +2,28 @@ let express = require('express');
 let path = require('path');
 let fs = require('fs');
 let MongoClient = require('mongodb').MongoClient;
-let bodyParser = require('body-parser');
 let app = express();
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
+// bodyParser was deprecated: https://stackoverflow.com/a/59892173
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
 
 app.get('/profile-picture', function (req, res) {
   let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
   res.writeHead(200, {'Content-Type': 'image/jpg' });
   res.end(img, 'binary');
 });
+
+let mongoUrl = "mongodb://admin:password@localhost:27017";
+
+// Check if an environment variable for MongoDB URL is set
+if (process.env.MONGO_URL) {
+  mongoUrl = process.env.MONGO_URL;
+}
 
 // use when starting application locally with node command
 let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
@@ -29,29 +34,53 @@ let mongoUrlDocker = "mongodb://admin:password@host.docker.internal:27017";
 // use when starting application as docker container, part of docker-compose
 let mongoUrlDockerCompose = "mongodb://admin:password@mongodb";
 
+let mongoUrlK8s = `mongodb://${process.env.USER_NAME}:${process.env.USER_PWD}@${process.env.DB_URL}`
+
 // pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
 let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 
 // "user-account" in demo with docker. "my-db" in demo with docker-compose
 let databaseName = "my-db";
 
+app.post('/update-profile', function (req, res) {
+  let userObj = req.body;
 
+  MongoClient.connect(mongoUrl , mongoClientOptions, function (err, client) {
+    if (err) throw err;
+
+    let db = client.db(databaseName);
+    userObj['userid'] = 1;
+
+    let myquery = { userid: 1 };
+    let newvalues = { $set: userObj };
+
+    db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
+      if (err) throw err;
+      client.close();
+    });
+
+  });
+  // Send response
+  res.send(userObj);
+});
 
 app.get('/get-profile', function (req, res) {
+  let response = {};
   // Connect to the db
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
+  MongoClient.connect(mongoUrl , mongoClientOptions, function (err, client) {
     if (err) throw err;
 
     let db = client.db(databaseName);
 
     let myquery = { userid: 1 };
 
-    db.collection("users").findOne(myquery, function (findErr, result) {
-      if (findErr) throw findErr;
+    db.collection("users").findOne(myquery, function (err, result) {
+      if (err) throw err;
+      response = result;
       client.close();
 
       // Send response
-      res.send(result ? result : {});
+      res.send(response ? response : {});
     });
   });
 });
